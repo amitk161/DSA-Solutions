7f883563f4d26cf610ab3987c33baf2f8a1763c6
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2022-12-22 22:30:16 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.java
+++ Number of Provinces - GFG/number-of-provinces.java
@@ -33,29 +33,29 @@ class GFG {
 
 class Solution {
     static int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
-       ArrayList<ArrayList<Integer>> adjLs = new ArrayList<>();
-       for(int i=0; i<V; i++){
-           adjLs.add(new ArrayList<Integer>());
-       }
-       
-       for(int i=0; i<V; i++){
-           for(int j=0; j<V; j++){
-               if(adj.get(i).get(j) == 1 && i != j){
-                   adjLs.get(i).add(j);
-                   adjLs.get(j).add(i);
-               }
-           }
-       }
-       
-       int[] vis = new int[V];
-       int cnt = 0;
-       for(int i=0; i<V; i++){
-           if(vis[i] == 0){
-               cnt++;
-               dfs(i, adjLs, vis);
-           }
-       }
-       return cnt;
+        ArrayList<ArrayList<Integer>> adjLs = new ArrayList<>();
+        for(int i=0; i<V; i++){
+            adjLs.add(new ArrayList<>());
+        }
+        
+        for(int i=0; i<V; i++){
+            for(int j=0; j<V; j++){ 
+                if(adj.get(i).get(j) == 1 && i != j){
+                    adjLs.get(i).add(j);
+                    adjLs.get(j).add(i);
+                }
+            }
+        }
+        
+        int[] vis = new int[V+1];
+        int cnt = 0;
+        for(int i=0; i<V; i++){
+            if(vis[i] == 0){
+                cnt++;
+                dfs(i, adjLs, vis);
+            }
+        }
+        return cnt;
     }
     
     private static void dfs(int node, ArrayList<ArrayList<Integer>> adjLs, int[] vis){
