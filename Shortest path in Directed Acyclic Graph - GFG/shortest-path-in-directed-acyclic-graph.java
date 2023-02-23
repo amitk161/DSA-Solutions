3c6a2fa0bda2e104cf35fd22c276fe25152cb467
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2023-02-23 19:36:57 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.java
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.java
@@ -44,7 +44,7 @@ class Solution {
 	public int[] shortestPath(int N,int M, int[][] edges) {
 		ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
 		for(int i=0; i<N; i++){
-		    adj.add(new ArrayList<Pair>());
+		    adj.add(new ArrayList<>());
 		}
 		
 		for(int i=0; i<M; i++){
@@ -54,20 +54,21 @@ class Solution {
 		    adj.get(u).add(new Pair(v, wt));
 		}
 		
-		int[] vis = new int[N];
 		Stack<Integer> st = new Stack<>();
+		int[] vis = new int[N];
 		for(int i=0; i<N; i++){
 		    if(vis[i] == 0){
-		        topoSort(i, adj, vis, st);
+		        topoSort(i, vis, adj, st);
 		    }
 		}
 		
-		int[] dist = new int[N];
+		int[] dis = new int[N];
 		for(int i=0; i<N; i++){
-		    dist[i] = (int)(1e9);
+		    dis[i] = (int)(1e9);
 		}
 		
-		dist[0] = 0;
+		dis[0] = 0;
+		
 		while(!st.isEmpty()){
 		    int node = st.peek();
 		    st.pop();
@@ -76,27 +77,27 @@ class Solution {
 		        int v = adj.get(node).get(i).first;
 		        int wt = adj.get(node).get(i).second;
 		        
-		        if(dist[node] + wt < dist[v]){
-		            dist[v] = dist[node] + wt;
+		        if(dis[node] + wt < dis[v]){
+		            dis[v] = dis[node] + wt;
 		        }
 		    }
 		}
 		
 		for(int i=0; i<N; i++){
-		    if(dist[i] == 1e9){
-		        dist[i] = -1;
+		    if(dis[i] == 1e9){
+		        dis[i] = -1;
 		    }
 		}
-		return dist;
+		return dis;
 	}
 	
-	private void topoSort(int node, ArrayList<ArrayList<Pair>> adj, int[] vis, Stack<Integer> st){
+	private void topoSort(int node, int[] vis, ArrayList<ArrayList<Pair>> adj, Stack<Integer> st){
 	    vis[node] = 1;
 	    
 	    for(int i=0; i<adj.get(node).size(); i++){
 	        int v = adj.get(node).get(i).first;
 	        if(vis[v] == 0){
-	            topoSort(v, adj, vis, st);
+	            topoSort(v, vis, adj, st);
 	        }
 	    }
 	    st.push(node);
