960a86236449394d28f7b317a155e6cae6b14570
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2023-03-21 13:58:55 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (21.13%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,30 +1,24 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
-        
-        int row = matrix.length;
-        int col = matrix[0].length;
+        int check = 1, row = matrix.length, col = matrix[0].length;
         
         for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
+            if(matrix[i][0] == 0) check = 0;
+            for(int j=1; j<col; j++){
                 if(matrix[i][j] == 0){
-                    list1.add(i);
-                    list2.add(j);
+                    matrix[i][0] = matrix[0][j] = 0;
                 }
             }
         }
         
-        for(int m=0; m<list1.size(); m++){
-            for(int p=0; p<col; p++){
-                matrix[list1.get(m)][p] = 0;
+        for(int i=row-1; i>=0; i--){
+            for(int j=col-1; j>=1; j--){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+                
             }
-        }
-        
-        for(int n=0; n<list2.size(); n++){
-            for(int q=0; q<row; q++){
-                matrix[q][list2.get(n)] = 0;
-            }
+            if(check == 0) matrix[i][0] = 0;
         }
     }
 }
