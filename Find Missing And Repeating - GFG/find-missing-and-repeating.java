fa2ef32b4150ddb6e657cfd0edc080cc1c006a6c
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2023-03-23 19:57:14 +0530

    Added solution - LeetHub

---
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().split(" ");
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            int[] ans = new Solve().findTwoElement(a, n);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+        int[] dup = new int[n+1];
+        int[] res = new int[2];
+        
+        for(int i=0; i<n; i++){
+            if(dup[arr[i]] == 0){
+                dup[arr[i]] = 1;
+            } else {
+                dup[arr[i]] = 2;
+            }
+        }
+        
+        for(int i=1; i<dup.length; i++){
+            if(dup[i] == 2){
+                res[0] = i;
+            } else if(dup[i] == 0){
+                res[1] = i;
+            }
+        }
+        return res;
+    }
+}
