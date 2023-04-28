c2c218b3f37ca9ef907fe3e2d4deaadefa90d218
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2023-04-28 17:11:56 +0530

    Time: 2 ms (100.00%), Space: 43.5 MB (64.96%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,34 +1,33 @@
 class Solution {
-    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
-        int m = arr1.length;
-        int n = arr2.length;
-        int[] finalArray = new int[m+n];
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
         
-        int i=0, j=0, k=0;
-        while(i < m && j < n){
-            if(arr1[i] < arr2[j]){
-                finalArray[k++] = arr1[i++];
-            } else {
-                finalArray[k++] = arr2[j++];
-            }
+        if(m > n)
+            return findMedianSortedArrays(nums2, nums1);
+        
+        int low = 0, high = m;
+        int median = (m + n + 1) / 2;
+        
+        while(low <= high){
+            int cut1 = (low + high) / 2;
+            int cut2 = median - cut1;
+            
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1-1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2-1];
+            int r1 = (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
+            int r2 = (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
+            
+            if(l1 <= r2 && l2 <= r1){
+                if((m + n) % 2 != 0)
+                    return Math.max(l1, l2);
+                else
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+            } else if(l1 > l2)
+                high = cut1 - 1;
+              else
+                low = cut1 + 1;
         }
-        
-        if(i < m){
-            while(i < m){
-                finalArray[k++] = arr1[i++];
-            }
-        }
-        
-        if(j < n){
-            while(j < n){
-                finalArray[k++] = arr2[j++];
-            }
-        }
-        
-        n = n + m;
-        if(n % 2 == 1)
-            return finalArray[((n+1)/2) - 1];
-        else
-            return ((double) finalArray[(n/2) - 1] + finalArray[(n/2)]) / 2.0;
+        return 0.0;
     }
 }
