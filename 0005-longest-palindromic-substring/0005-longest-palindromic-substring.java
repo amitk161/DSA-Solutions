d04fa1769d5ce611826260160a9cd1c0ef16c0be
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2023-05-05 14:18:51 +0530

    Time: 24 ms (61.08%), Space: 42.5 MB (70.18%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,26 +1,45 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int start = 0, end = 0;
-        for(int i=0; i<s.length(); i++){
-            int odd = expand(s, i, i);
-            int even = expand(s, i, i+1);
-            
-            int len = Math.max(odd, even);
-            
-            if(len > end-start){
-                start = i - (len-1)/2;
-                end = i + len/2;
+        if(s.length() < 1)
+            return s;
+        int max = 1;
+        int n = s.length();
+        int st = 0, end = 0;
+        
+        for(int i=0; i<n-1; i++){
+            int l = i, r = i;
+            while(l >= 0 && r < n){
+                if(s.charAt(l) == s.charAt(r)){
+                    l--; r++;
+                } else {
+                    break;
+                }
+            }
+            int len = r-l-1;
+            if(len > max){
+                max = len;
+                st = l+1;
+                end = r-1;
             }
         }
-        return s.substring(start, end+1);
-    }
-    
-    public int expand(String s, int left, int right){
-        while(left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-            left--;
-            right++;
+        
+        for(int i=0; i<n-1; i++){
+            int l = i, r = i+1;
+            while(l >= 0 && r < n){
+                if(s.charAt(l) == s.charAt(r)){
+                    l--; r++;
+                } else {
+                    break;
+                }
+            }
+            int len = r-l-1;
+            if(len > max){
+                max = len;
+                st = l+1;
+                end = r-1;
+            }
         }
         
-        return right - left - 1;
+        return s.substring(st, end+1);
     }
 }
