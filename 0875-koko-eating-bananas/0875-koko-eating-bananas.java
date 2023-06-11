13b3765fb4bdfce5a885f8bba9d77d0fea18fa68
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2023-06-11 19:16:51 +0530

    Time: 17 ms (75.99%), Space: 44.5 MB (23.94%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,16 +1,37 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int H) {
-        int low = 1, high = 1000000000, k = 0;
-        while (low <= high) {
-            k = (low + high) / 2;
-            int h = 0;
-            for (int i = 0; i < piles.length; i ++) 
-                h += Math.ceil(1.0 * piles[i] / k);
-            if (h > H)
-                low = k + 1;
-            else
-                high = k - 1;
+    public int minEatingSpeed(int[] piles, int h) {
+        int start = 1;
+        int end = Integer.MIN_VALUE; // for finding largest value in piles
+
+        // finding max
+        for(int i=0;i<piles.length;i++){
+            if(piles[i]>end){
+                end = piles[i];
+            }
         }
-        return low;
+        int ans = -1;
+        // apply binary search on the possible values of K -> (1, max(piles))
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(isEatingSpeedValid(piles, mid, h)){
+                ans = mid;
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+    
+    public boolean isEatingSpeedValid(int piles[], int eatingSpeed, int hours) {
+        int hoursSpent = 0;
+        for(int i=0;i<piles.length;i++){
+            hoursSpent += piles[i]/eatingSpeed;
+            if(piles[i]%eatingSpeed!=0){
+                hoursSpent += 1;
+            }
+            if(hoursSpent>hours) return false;
+        }
+        return true;
     }
 }
