f630592c2c3e0eda04ce947c5e4506439b539102
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2023-06-11 19:11:01 +0530

    Added solution - LeetHub

---
+++ Koko Eating Bananas - GFG/koko-eating-bananas.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for(int i=0;i<n;i++){
+                a[i]=sc.nextInt();
+            }
+            int h = sc.nextInt();
+            Solution obj = new Solution();
+            int ans = obj.Solve(n,a,h);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int Solve(int N, int[] piles, int H) {
+        int low = 1, high = 1000000000, k = 0;
+        while (low <= high) {
+            k = (low + high) / 2;
+            int h = 0;
+            for (int i = 0; i < piles.length; i ++) 
+                h += Math.ceil(1.0 * piles[i] / k);
+            if (h > H)
+                low = k + 1;
+            else
+                high = k - 1;
+        }
+        return low;
+    }
+}
+        
