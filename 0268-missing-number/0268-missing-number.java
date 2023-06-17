539ca2e46dee9647898640420dfd1cb17d0fcab8
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2023-06-17 12:53:59 +0530

    Time: 186 ms (5.15%), Space: 44.4 MB (23.44%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int missingNumber(int[] arr) {
+        int n = arr.length+1;
+        
+        for(int i=0; i<=n; i++){
+            int flag = 0;
+            for(int j=0; j<n-1; j++){
+                if(arr[j] == i){
+                    flag = 1;
+                    break;
+                }
+            }
+            
+            if(flag == 0)
+                return i;
+        }
+        return -1;
+    }
+}
