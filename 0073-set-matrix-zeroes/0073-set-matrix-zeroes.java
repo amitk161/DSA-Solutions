55843e5c115e1dfc4585aa0529823c4aa337b008
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2023-07-08 18:16:32 +0530

    Time: 1 ms (75.20%), Space: 44.3 MB (87.53%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,9 +1,9 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int check = 1, row = matrix.length, col = matrix[0].length;
+        int col0 = 1, row = matrix.length, col = matrix[0].length;
         
         for(int i=0; i<row; i++){
-            if(matrix[i][0] == 0) check = 0;
+            if(matrix[i][0] == 0) col0 = 0;
             for(int j=1; j<col; j++){
                 if(matrix[i][j] == 0){
                     matrix[i][0] = matrix[0][j] = 0;
@@ -16,9 +16,8 @@ class Solution {
                 if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
-                
             }
-            if(check == 0) matrix[i][0] = 0;
+            if(col0 == 0) matrix[i][0] = 0;
         }
     }
 }
