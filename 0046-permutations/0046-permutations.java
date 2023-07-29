783b033041acd2181aa7b1b34fa5277b375169c9
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2023-07-29 18:57:12 +0530

    Time: 1 ms (98.80%), Space: 43.3 MB (99.13%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,25 +1,30 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        boolean[] freq = new boolean[nums.length];
-        recur(nums, freq, new ArrayList<>(), ans);
+        recur(0, nums, ans);
         return ans;
     }
     
-    private void recur(int[] nums, boolean[] freq, List<Integer> ds, List<List<Integer>> ans){
-        if(ds.size() == nums.length){
+    private void recur(int ind, int[] nums, List<List<Integer>> ans){
+        if(ind == nums.length){
+            List<Integer> ds = new ArrayList<>();
+            for(int i=0; i<nums.length; i++){
+                ds.add(nums[i]);
+            }
             ans.add(new ArrayList<>(ds));
             return;
         }
         
-        for(int i=0; i<nums.length; i++){
-            if(freq[i] == false){
-                freq[i] = true;
-                ds.add(nums[i]);
-                recur(nums, freq, ds, ans);
-                ds.remove(ds.size() - 1);
-                freq[i] = false;
-            }
+        for(int i=ind; i<nums.length; i++){
+            swap(i, ind, nums);
+            recur(ind+1, nums, ans);
+            swap(i, ind, nums);
         }
+    }
+    
+    private void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
