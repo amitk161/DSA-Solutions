28c09d209adbfbbb94c89ee142f62999a421c222
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2023-08-06 22:45:19 +0530

    Time: 4 ms (6.20%), Space: 40.5 MB (24.28%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        HashSet<List<Integer>> ans = new HashSet<>();
+        findCombinations(1, 0, 0, k, n, new ArrayList<>(), ans);
+        return new ArrayList<>(ans);
+    }
+    
+    private void findCombinations(int ind, int count, int s, int k, int sum, 
+        List<Integer> ds, HashSet<List<Integer>> ans){
+        if(count == k){
+            if(s == sum){
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+        
+        for(int i=ind; i<=9; i++){
+            if(count < k){
+                ds.add(i);
+                findCombinations(i+1, count+1, s + i, k, sum, ds, ans);
+                ds.remove(ds.size() - 1);
+            }
+            findCombinations(i+1, count, s, k, sum, ds, ans);
+        }
+    }
+}
