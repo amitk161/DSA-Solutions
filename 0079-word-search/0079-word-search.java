09aa198c9345de536469358c82f10a5b35378387
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2023-08-07 15:11:34 +0530

    Time: 140 ms (90.81%), Space: 40.7 MB (59.17%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,44 +1,37 @@
 class Solution {
+    static boolean[][] visited;
     public boolean exist(char[][] board, String word) {
-        int n = board.length;
-        int m = board[0].length;
-        int[][] vis = new int[n][m];
+        visited = new boolean[board.length][board[0].length];
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(board[i][j] == word.charAt(0)){
-                    if(dfs(i, j, vis, board, word, 0))
-                        return true;
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if((word.charAt(0) == board[i][j]) && search(board, word, i, j, 0)){
+                    return true;
                 }
             }
         }
+        
         return false;
     }
     
-    private boolean dfs(int row, int col, int[][] vis, char[][] board, String word, int count) {
-        int n = board.length;
-        int m = board[0].length;
-        
-        int[] delRow = {-1, 0, 1, 0};
-        int[] delCol = {0, 1, 0, -1};
-        vis[row][col] = 1;
-        
-        if(count + 1 == word.length())
+    private boolean search(char[][]board, String word, int i, int j, int index){
+        if(index == word.length()){
             return true;
-        
-        for(int i=0; i<4; i++){
-            int nRow = row + delRow[i];
-            int nCol = col + delCol[i];
-            int ind = count + 1;
-            
-            if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && vis[nRow][nCol] == 0
-              && board[nRow][nCol] == word.charAt(ind)){
-                if(dfs(nRow, nCol, vis, board, word, ind))
-                    return true;
-            }
         }
         
-        vis[row][col] = 0;
+        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index) || visited[i][j]){
+            return false;
+        }
+        
+        visited[i][j] = true;
+        if(search(board, word, i-1, j, index+1) || 
+           search(board, word, i+1, j, index+1) ||
+           search(board, word, i, j-1, index+1) || 
+           search(board, word, i, j+1, index+1)){
+            return true;
+        }
+        
+        visited[i][j] = false;
         return false;
     }
 }
