2439a1eec09b4a8d9861a22c9b638f1644c8e83a
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2023-09-15 12:54:51 +0530

    Time: 4 ms (46.84%), Space: 43.8 MB (71.80%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -3,25 +3,18 @@ class Solution {
         int n = triangle.size();
         int[][] dp = new int[n+1][n+1];
         
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        } 
-        
-        return f(0, 0, n, triangle, dp);
-    }
-    
-    private int f(int i, int j, int n, List<List<Integer>> triangle, int[][] dp){
-        if(i == n-1){
-            return triangle.get(n-1).get(j);
+        for(int j=0; j<n; j++){
+            dp[n-1][j] = triangle.get(n-1).get(j);
         }
         
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int d = triangle.get(i).get(j) + f(i+1, j, n, triangle, dp);
-        int dg = triangle.get(i).get(j) + f(i+1, j+1, n, triangle, dp);
-        
-        return dp[i][j] = Math.min(d, dg);
+        for(int i=n-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                int d = triangle.get(i).get(j) + dp[i+1][j];
+                int dg = triangle.get(i).get(j) + dp[i+1][j+1];
+                
+                dp[i][j] = Math.min(d, dg);
+            }
+        }
+        return dp[0][0];
     }
 }
