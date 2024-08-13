f1b7a32181a1ed40d9bb3b1d0c591c4bd2408b4f
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-08-13 17:37:59 +0530

    Time: 0 ms (100%), Space: 49.9 MB (85.36%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -6,7 +6,7 @@ class Solution {
         if(nums[0] != nums[1]) return nums[0];
         if(nums[n-1] != nums[n-2]) return nums[n-1];
 
-        int lo = 0, hi = n-2;
+        int lo = 1, hi = n-2;
 
         while(lo <= hi){
             int mid = (lo + hi) / 2;
