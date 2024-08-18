3415e83a612d3f26e35bf3a9ea44e893bd3718c8
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-08-18 21:10:34 +0530

    Time: 2 ms (63.35%), Space: 45.9 MB (64.91%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,31 +1,45 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length, n2 = nums2.length;
-        int i = 0, j = 0;
-        ArrayList<Integer> list = new ArrayList<>();
+        int n = n1 + n2;
 
+        int ind2 = n / 2;
+        int ind1 = ind2 - 1;
+        int cnt = 0;
+        int ind1ele = -1, ind2ele = -1;
+
+        int i=0, j=0;
         while(i < n1 && j < n2){
             if(nums1[i] <= nums2[j]){
-                list.add(nums1[i]);
+                if(cnt == ind1) ind1ele = nums1[i];
+                if(cnt == ind2) ind2ele = nums1[i];
+                cnt++;
                 i++;
             } else {
-                list.add(nums2[j]);
+                if(cnt == ind1) ind1ele = nums2[j];
+                if(cnt == ind2) ind2ele = nums2[j];
+                cnt++;
                 j++;
             }
         }
 
         while(i < n1){
-            list.add(nums1[i++]);
+            if(cnt == ind1) ind1ele = nums1[i];
+            if(cnt == ind2) ind2ele = nums1[i];
+            cnt++;
+            i++;
         }
 
         while(j < n2){
-            list.add(nums2[j++]);
+            if(cnt == ind1) ind1ele = nums2[j];
+            if(cnt == ind2) ind2ele = nums2[j];
+            cnt++;
+            j++;
         }
 
-        int n = n1 + n2;
         if(n % 2 == 1)
-            return (double) list.get(n/2);
+            return (double) ind2ele;
 
-        return ((double) (list.get(n / 2)) + (double) (list.get((n / 2) - 1))) / 2.0;
+        return (double) ((double) (ind1ele + ind2ele)) / 2.0;
     }
 }
