27a28d3c21e7dac8c559922e3cf03ec8e2b7127a
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-08-18 20:18:44 +0530

    Time: 2 ms (63.35%), Space: 46.2 MB (22.89%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,33 +1,31 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        
-        if(m > n)
-            return findMedianSortedArrays(nums2, nums1);
-        
-        int low = 0, high = m;
-        int median = (m + n + 1) / 2;
-        
-        while(low <= high){
-            int cut1 = (low + high) / 2;
-            int cut2 = median - cut1;
-            
-            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1-1];
-            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2-1];
-            int r1 = (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
-            int r2 = (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
-            
-            if(l1 <= r2 && l2 <= r1){
-                if((m + n) % 2 != 0)
-                    return Math.max(l1, l2);
-                else
-                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
-            } else if(l1 > l2)
-                high = cut1 - 1;
-              else
-                low = cut1 + 1;
+        int n1 = nums1.length, n2 = nums2.length;
+        int i = 0, j = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while(i < n1 && j < n2){
+            if(nums1[i] <= nums2[j]){
+                list.add(nums1[i]);
+                i++;
+            } else {
+                list.add(nums2[j]);
+                j++;
+            }
         }
-        return 0.0;
+
+        while(i < n1){
+            list.add(nums1[i++]);
+        }
+
+        while(j < n2){
+            list.add(nums2[j++]);
+        }
+
+        int n = n1 + n2;
+        if(n % 2 == 1)
+            return (double) list.get(n/2);
+
+        return ((double) (list.get(n / 2)) + (double) (list.get((n / 2) - 1))) / 2.0;
     }
 }
