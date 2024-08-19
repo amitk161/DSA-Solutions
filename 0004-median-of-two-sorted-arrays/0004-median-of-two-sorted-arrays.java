21ba9f61a5933dd96061f1513a742b25f73ad9f4
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-08-19 18:28:21 +0530

    Time: 1 ms (100%), Space: 45.8 MB (64.98%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,45 +1,32 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length;
-        int n = n1 + n2;
+        int m = nums1.length, n = nums2.length;
 
-        int ind2 = n / 2;
-        int ind1 = ind2 - 1;
-        int cnt = 0;
-        int ind1ele = -1, ind2ele = -1;
+        if(m > n)
+            return findMedianSortedArrays(nums2, nums1);
 
-        int i=0, j=0;
-        while(i < n1 && j < n2){
-            if(nums1[i] <= nums2[j]){
-                if(cnt == ind1) ind1ele = nums1[i];
-                if(cnt == ind2) ind2ele = nums1[i];
-                cnt++;
-                i++;
-            } else {
-                if(cnt == ind1) ind1ele = nums2[j];
-                if(cnt == ind2) ind2ele = nums2[j];
-                cnt++;
-                j++;
-            }
-        }
+        int lo = 0, hi = m;
+        int median = (m + n + 1) / 2;
 
-        while(i < n1){
-            if(cnt == ind1) ind1ele = nums1[i];
-            if(cnt == ind2) ind2ele = nums1[i];
-            cnt++;
-            i++;
-        }
+        while(lo <= hi){
+            int cut1 = (lo + hi) / 2;
+            int cut2 = median - cut1;
 
-        while(j < n2){
-            if(cnt == ind1) ind1ele = nums2[j];
-            if(cnt == ind2) ind2ele = nums2[j];
-            cnt++;
-            j++;
-        }
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1-1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2-1];
+            int r1 = (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
+            int r2 = (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
 
-        if(n % 2 == 1)
-            return (double) ind2ele;
-
-        return (double) ((double) (ind1ele + ind2ele)) / 2.0;
+            if(l1 <= r2 && l2 <= r1){
+                if((m + n) % 2 == 1)
+                    return Math.max(l1, l2);
+                else
+                    return (double) (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+            } else if(l1 > r2)
+                hi = cut1 - 1;
+               else 
+                lo = cut1 + 1; 
+        }   
+        return 0.0;
     }
 }
