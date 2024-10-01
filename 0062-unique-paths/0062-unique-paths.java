14cc2d439021610ff09a9d0b2c7cbab7593fa12e
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-10-02 00:51:50 +0530

    Time: 0 ms (100%), Space: 40.2 MB (77.64%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,22 +4,22 @@ class Solution {
 
         for(int[] row: dp){
             Arrays.fill(row, -1);
-        }    
+        }
 
         return count(0, 0, m, n, dp);
     }
 
-    private int count(int i, int j, int m, int n, int[][] dp){
+    public int count(int i, int j, int m, int n, int[][] dp){
         if(i == m-1 && j == n-1)
             return 1;
 
-        if(i >= m || j >= n)
+        if(i > m || j > n)
             return 0;
 
         if(dp[i][j] != -1)
             return dp[i][j];    
 
-        dp[i][j] = count(i, j+1, m, n, dp) + count(i+1, j, m, n, dp);        
+        dp[i][j] = count(i, j+1, m, n, dp) + count(i+1, j, m, n, dp);    
 
         return dp[i][j];
     }
