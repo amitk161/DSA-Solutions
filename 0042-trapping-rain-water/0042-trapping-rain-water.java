5a53fb03b8656cfb10c15cd724e3ea208fee7842
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-10-04 01:13:12 +0530

    Time: 1 ms (66.97%), Space: 46.2 MB (57.22%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,27 +1,24 @@
 class Solution {
     public int trap(int[] arr) {
         int n = arr.length;
-        int left = 0, right = n-1;
-        int leftMax = 0, rightMax = 0;
-        int res = 0;
-        
-        while(left <= right){
-            if(arr[left] <= arr[right]){
-                if(arr[left] >= leftMax){
-                    leftMax = arr[left];
-                } else {
-                    res += leftMax - arr[left];
-                }
-                left++;
-            } else {
-                if(arr[right] >= rightMax){
-                    rightMax = arr[right];
-                } else {
-                    res += rightMax - arr[right];
-                }
-                right--;
-            }
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+
+        prefix[0] = arr[0];
+        for(int i=1; i<n; i++){
+            prefix[i] = Math.max(prefix[i-1], arr[i]);
         }
-        return res;
+
+        suffix[n-1] = arr[n-1];
+        for(int i=n-2; i>=0; i--){
+            suffix[i] = Math.max(suffix[i+1], arr[i]);
+        }
+
+        int water = 0;
+        for(int i=0; i<n; i++){
+            water += Math.min(prefix[i], suffix[i]) - arr[i];
+        }
+
+        return water;
     }
 }
