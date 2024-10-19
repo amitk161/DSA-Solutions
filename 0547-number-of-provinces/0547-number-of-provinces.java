e86620b0ceb7461f1c31f214c8689836f514a4a0
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-10-19 19:14:40 +0530

    Time: 4 ms (21.76%), Space: 46.5 MB (89.66%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,21 +1,39 @@
-public class Solution {
-    public void dfs(int[][] M, int[] visited, int i) {
-        for (int j = 0; j < M.length; j++) {
-            if (M[i][j] == 1 && visited[j] == 0) {
-                visited[j] = 1;
-                dfs(M, visited, j);
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int V = isConnected.length;
+
+        for(int i=0; i<V; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<V; i++){
+            for(int j=0; j<V; j++){
+                if(isConnected[i][j] == 1 && i != j){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
             }
         }
+
+        int[] vis = new int[V];
+        int cnt = 0;
+        for(int i=0; i<V; i++){
+            if(vis[i] == 0){
+                cnt++;
+                dfs(i, adj, vis);
+            }
+        }
+        return cnt;
     }
-    public int findCircleNum(int[][] M) {
-        int[] visited = new int[M.length];
-        int count = 0;
-        for (int i = 0; i < M.length; i++) {
-            if (visited[i] == 0) {
-                dfs(M, visited, i);
-                count++;
+
+    public void dfs(int node, ArrayList<ArrayList<Integer>> adj, int[] vis){
+        vis[node] = 1;
+
+        for(Integer it: adj.get(node)){
+            if(vis[it] == 0){
+                dfs(it, adj, vis);
             }
         }
-        return count;
     }
 }
