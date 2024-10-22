412b990bb62ed9986d9a2828eb87d6a3c07b7440
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-10-22 22:04:43 +0530

    Time: 3 ms (82.21%), Space: 46.2 MB (5.7%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,25 +1,33 @@
 class Solution {
     public int candy(int[] ratings) {
+        int sum = 1, i = 1;
         int n = ratings.length;
-        int[] left = new int[n];
-        left[0] = 1;
 
-        for(int i=1; i<n; i++){
-            if(ratings[i] > ratings[i-1]){
-                left[i] = left[i-1] + 1;
-            } else {
-                left[i] = 1;
+        while(i < n){
+            if(ratings[i] == ratings[i-1]){
+                sum += 1;
+                i++;
+                continue;
             }
-        }
 
-        int right = 1, sum = left[n-1];
-        for(int i=n-2; i>=0; i--){
-            if(ratings[i] > ratings[i+1]){
-                right++;
-            } else {
-                right = 1;
+            int peak = 1;
+            while(i < n && ratings[i] > ratings[i-1]){
+                peak++;
+                sum += peak;
+                i++;
             }
-            sum += Math.max(left[i], right);
+
+            int down = 0;
+            while(i < n && ratings[i] < ratings[i-1]){
+                down++;
+                sum += down;
+                i++;
+            }
+            down++;
+            
+            if(down > peak){
+                sum += (down - peak);
+            }
         }
         return sum;
     }
