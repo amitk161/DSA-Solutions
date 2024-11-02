6b9b5e28a6f3d7a8600e417d1842b8f267d419a0
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-11-03 00:13:26 +0530

    Time: 27 ms (95.43%), Space: 47.3 MB (87.79%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,92 +1,75 @@
-class DisjointSet {
-    List<Integer> rank = new ArrayList<>();
-    List<Integer> parent = new ArrayList<>();
-    List<Integer> size = new ArrayList<>(); 
-    public DisjointSet(int n) {
-        for(int i = 0;i<=n;i++) {
-            rank.add(0); 
-            parent.add(i); 
-            size.add(1); 
-        }
-    }
+class DisjointSet{
+	List<Integer> parent = new ArrayList<>();
+	List<Integer> size = new ArrayList<>();
 
-    public int findUPar(int node) {
-        if(node == parent.get(node)) {
-            return node; 
-        }
-        int ulp = findUPar(parent.get(node)); 
-        parent.set(node, ulp); 
-        return parent.get(node); 
-    }
+	public DisjointSet(int n){
+		for(int i=0; i<=n; i++){
+			size.add(1);
+			parent.add(i);
+		}
+	}
 
-    public void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u); 
-        int ulp_v = findUPar(v); 
-        if(ulp_u == ulp_v) return; 
-        if(rank.get(ulp_u) < rank.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v); 
-        }
-        else if(rank.get(ulp_v) < rank.get(ulp_u)) {
-            parent.set(ulp_v, ulp_u); 
-        }
-        else {
-            parent.set(ulp_v, ulp_u); 
-            int rankU = rank.get(ulp_u); 
-            rank.set(ulp_u, rankU + 1); 
-        }
-    }
-    
-    public void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u); 
-        int ulp_v = findUPar(v); 
-        if(ulp_u == ulp_v) return; 
-        if(size.get(ulp_u) < size.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v); 
-            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u)); 
-        }
-        else {
-            parent.set(ulp_v, ulp_u); 
-            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
-        }
-    }
+	public int findPar(int node){
+		if(node == parent.get(node)){
+			return node;
+		}
+
+		int ulp = findPar(parent.get(node));
+		parent.set(node, ulp);
+		return parent.get(node);
+	}
+
+	public void unionBySize(int u, int v){
+		int ulp_u = findPar(u);
+		int ulp_v = findPar(v);
+		if(ulp_u == ulp_v)
+			return;
+
+		if(size.get(ulp_u) < size.get(ulp_v)){
+			parent.set(ulp_u, ulp_v);
+			size.set(ulp_v, size.get(ulp_u) + size.get(ulp_v));
+		} else {
+			parent.set(ulp_v, ulp_u);
+			size.set(ulp_u, size.get(ulp_v) + size.get(ulp_u)); 
+		}		
+	}
 }
 
-
 class Solution {
-    public List<List<String>> accountsMerge(List<List<String>> details) {
-        int n = details.size();
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        int n = accounts.size();
         DisjointSet ds = new DisjointSet(n);
-        HashMap<String, Integer> mailNode = new HashMap<>(); 
-        
+        HashMap<String, Integer> mapMailNode = new HashMap<>();
+
         for(int i=0; i<n; i++){
-            for(int j=1; j<details.get(i).size(); j++){
-                String mail = details.get(i).get(j);
-                if(mailNode.containsKey(mail) == false){
-                    mailNode.put(mail, i);
+            for(int j=1; j<accounts.get(i).size(); j++){
+                String mail = accounts.get(i).get(j);
+                if(!mapMailNode.containsKey(mail)){
+                    mapMailNode.put(mail, i);
                 } else {
-                    ds.unionByRank(i, mailNode.get(mail));
+                    ds.unionBySize(i, mapMailNode.get(mail));
                 }
             }
         }
-        
+
         ArrayList<String>[] mergedMail = new ArrayList[n];
         for(int i=0; i<n; i++){
             mergedMail[i] = new ArrayList<String>();
         }
-        
-        for(Map.Entry<String, Integer> it : mailNode.entrySet()){
+
+        for(Map.Entry<String, Integer> it: mapMailNode.entrySet()){
             String mail = it.getKey();
-            int node = ds.findUPar(it.getValue());
+            int node = ds.findPar(it.getValue());
             mergedMail[node].add(mail);
         }
-        
+
         List<List<String>> ans = new ArrayList<>();
-        
+
         for(int i=0; i<n; i++){
             if(mergedMail[i].size() == 0) continue;
             Collections.sort(mergedMail[i]);
             List<String> temp = new ArrayList<>();
-            temp.add(details.get(i).get(0));
+            temp.add(accounts.get(i).get(0));
             for(String it: mergedMail[i]){
                 temp.add(it);
             }
