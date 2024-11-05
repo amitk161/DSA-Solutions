baefc1484946fd945ec22d1b376ae165b814635b
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-11-06 00:42:49 +0530

    Time: 42 ms (30.39%), Space: 66.9 MB (6.13%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -1,29 +1,20 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int n = arr.length;
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
-            public int compare(int[] x, int[] y){
-                return x[0] - y[0];
-            }
-        });
-    
-        for(int i=0; i<n; i++){
-            pq.offer(new int[]{arr[i], i});
+        HashMap<Integer, Integer> map = new HashMap<>();
+        TreeSet<Integer> set = new TreeSet<>();
+        for(int ele: arr){
+            set.add(ele);
         }
 
-        int rank = 0;
-        int prev = Integer.MAX_VALUE;
-        
-        while(!pq.isEmpty()){
-            int[] curr = pq.poll();
-            if(prev != curr[0]){
-                prev = curr[0];
-                rank++;
-            }
-            arr[curr[1]] = rank;
+        int rank = 1;
+        for(int num: set){
+            map.put(num, rank);
+            rank++;
+        }
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = map.get(arr[i]);
         }
         return arr;
     }
 }
-
-
