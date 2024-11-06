871442a6db93b1f2f7764fc51ca13c638d6559c8
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-11-07 01:11:12 +0530

    Time: 14 ms (58.3%), Space: 45.5 MB (86.58%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,26 +1,31 @@
+class Pair{
+    int element;
+    int frequency;
+
+    public Pair(int element, int frequency){
+        this.element = element;
+        this.frequency = frequency;
+    }    
+}
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>((x, y) -> y.frequency - x.frequency);
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<nums.length; i++){
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        int ind = 0;
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            pq.offer(new Pair(entry.getKey(), entry.getValue()));
+        }
+
         int[] res = new int[k];
+        int ind = 0;
 
         while(k > 0){
-            int maxFreq = 0;
-            int ele = 0;
-
-            for(int it: map.keySet()){
-                if(map.get(it) > maxFreq){
-                    maxFreq = map.get(it);
-                    ele = it;
-                }
-            }
-
-            res[ind++] = ele;
-            map.remove(ele);
+            Pair it = pq.poll();
+            res[ind++] = it.element;
             k--;
         }
         return res;
