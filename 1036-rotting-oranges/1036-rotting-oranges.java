19f14d4b6a194008f268221f9e5fd200cf1fcecb
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2024-11-18 17:58:19 +0530

    Time: 2 ms (72.32%), Space: 42.5 MB (41.23%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,28 +1,28 @@
-class Pair {
-    int row;
-    int col;
-    int tm;
+class Pair{
+    int first;
+    int second;
+    int third;
 
-    public Pair(int row, int col, int tm){
-        this.row = row;
-        this.col = col;
-        this.tm = tm;
+    public Pair(int first, int second, int third){
+        this.first = first;
+        this.second = second;
+        this.third = third;
     }
 }
 class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+        int[][] vis = new int[n][m];
 
         Queue<Pair> q = new LinkedList<>();
-        int[][] vis = new int[n][m];
         int cntFresh = 0;
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j] == 2){
                     q.add(new Pair(i, j, 0));
-                    vis[i][j] = 2;
+                    vis[i][j] = 1;
                 }
 
                 if(grid[i][j] == 1){
@@ -37,19 +37,19 @@ class Solution {
         int cnt = 0;
 
         while(!q.isEmpty()){
-            int r = q.peek().row;
-            int c = q.peek().col;
-            int t = q.peek().tm;
-            tm = Math.max(tm, t);
+            int row = q.peek().first;
+            int col = q.peek().second;
+            int time = q.peek().third;
             q.remove();
+            tm = Math.max(tm, time);
 
             for(int i=0; i<4; i++){
-                int nrow = r + delrow[i];
-                int ncol = c + delcol[i];
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
 
-                if(nrow >=0 && nrow < n && ncol >= 0 && ncol < m
-                && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
-                    q.add(new Pair(nrow, ncol, t+1));
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
+                && vis[nrow][ncol] != 1 && grid[nrow][ncol] == 1){
+                    q.add(new Pair(nrow, ncol, time+1));
                     vis[nrow][ncol] = 1;
                     cnt++;
                 }
@@ -58,7 +58,6 @@ class Solution {
 
         if(cnt != cntFresh)
             return -1;
-
         return tm;    
     }
 }
