a7fd558c0e4fdedd7e05f07adc963403fd2bc387
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2025-02-18 23:57:49 +0530

    Time: 45 ms (11.8%), Space: 45.4 MB (11.98%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -8,15 +8,11 @@ class Solution {
             map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
             maxf = Math.max(maxf, map.get(s.charAt(r)));
 
-            while(r-l+1 - maxf > k){
+            if(r-l+1 - maxf > k){
                 map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
                 if(map.get(s.charAt(l)) == 0)
                     map.remove(s.charAt(l));
-                maxf = 0;
-
-                for(Map.Entry<Character, Integer> it: map.entrySet()){
-                    maxf = Math.max(maxf, it.getValue());
-                }    
+                maxf = 0;   
 
                 l++;
             }
