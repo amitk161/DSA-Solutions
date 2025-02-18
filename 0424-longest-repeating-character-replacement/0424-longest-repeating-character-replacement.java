9d5a6c7b1d70263f891729ec4be717d677e7b3a3
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2025-02-18 22:34:32 +0530

    Time: 48 ms (10.51%), Space: 45.3 MB (26.01%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,22 +1,30 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int maxLen = 0;
+        int maxLen = 0, maxf = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int l = 0, r = 0;
 
-        for(int i=0; i<s.length(); i++){
-            HashMap<Character, Integer> map = new HashMap<>();
-            int maxf = 0;
-            for(int j=i; j<s.length(); j++){
-                map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
+        while(r < s.length()){
+            map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
+            maxf = Math.max(maxf, map.get(s.charAt(r)));
 
-                maxf = Math.max(maxf, map.get(s.charAt(j)));
-                int changes = j-i+1 - maxf;
+            while(r-l+1 - maxf > k){
+                map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
+                if(map.get(s.charAt(l)) == 0)
+                    map.remove(s.charAt(l));
+                maxf = 0;
 
-                if(changes <= k){
-                    maxLen = Math.max(maxLen, j-i+1);
-                } else {
-                    break;
-                }
+                for(Map.Entry<Character, Integer> it: map.entrySet()){
+                    maxf = Math.max(maxf, it.getValue());
+                }    
+
+                l++;
             }
+
+            if(r-l+1 - maxf <= k){
+                maxLen = Math.max(maxLen, r-l+1);
+            }
+            r++;
         }
         return maxLen;
     }
