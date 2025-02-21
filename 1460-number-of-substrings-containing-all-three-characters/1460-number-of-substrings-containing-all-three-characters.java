1a57fcfd116f7b6a69a3e67f258cc497278962b5
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2025-02-21 11:41:27 +0530

    Time: 11 ms (91.11%), Space: 44.4 MB (95.1%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i=0; i<s.length(); i++){
             lastSeen[s.charAt(i) - 'a'] = i;
 
-            if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1){
+            if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -2){
                 cnt += 1 + Math.min(lastSeen[0], Math.min(lastSeen[1], lastSeen[2]));
             }
         }
