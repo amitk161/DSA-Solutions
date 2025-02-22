deecbf88047a862032ad6d333884dee353f81095
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2025-02-22 11:24:38 +0530

    Time: 13 ms (26.82%), Space: 55.3 MB (90.91%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,56 +10,22 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null)
-            return head;
+        ArrayList<Integer> list = new ArrayList<>();
+        ListNode temp = head;
 
-        ListNode middle = findMiddle(head);
-
-        ListNode left = head;
-        ListNode right = middle.next;
-        middle.next = null;
-
-        left = sortList(left);
-        right = sortList(right);
-
-        return mergeLL(left, right);
-    }
-
-    private ListNode findMiddle(ListNode head){
-        if(head == null || head.next == null)
-            return head;
-
-        ListNode slow = head;
-        ListNode fast = head.next;
-
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }    
-
-        return slow;
-    }
-
-    private ListNode mergeLL(ListNode list1, ListNode list2){
-        ListNode dummy = new ListNode();
-        ListNode temp = dummy;
-
-        while(list1 != null && list2 != null){
-            if(list1.val <= list2.val){
-                temp.next = list1;
-                list1 = list1.next;
-            } else {
-                temp.next = list2;
-                list2 = list2.next;
-            }
+        while(temp != null){
+            list.add(temp.val);
             temp = temp.next;
         }
 
-        if(list1 != null){
-            temp.next = list1;
-        } else {
-            temp.next = list2;
+        Collections.sort(list);
+        temp = head;
+        int i = 0;
+
+        while(temp != null){
+            temp.val = list.get(i++);
+            temp = temp.next;
         }
-        return dummy.next;
+        return head;
     }
 }
