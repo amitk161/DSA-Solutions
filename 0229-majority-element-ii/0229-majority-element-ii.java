cfa9fac6aba99a3df4c51f5883d76ec42143b2da
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2025-02-23 00:44:53 +0530

    Time: 2 ms (99.07%), Space: 48.5 MB (10.96%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,44 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int el1 = -1, el2 = -1, cnt1 = 0, cnt2 = 0;
+        int ele1 = 0, ele2 = 0, cnt1 = 0, cnt2 = 0;
         int n = nums.length;
-        
+
         for(int i=0; i<n; i++){
-            if(nums[i] == el1){
+            if(cnt1 == 0 && nums[i] != ele2){
+                ele1 = nums[i];
                 cnt1++;
-            } else if(nums[i] == el2){
+            } else if(cnt2 == 0 && nums[i] != ele1){
+                ele2 = nums[i];
                 cnt2++;
-            } else if(cnt1 == 0){
-                el1 = nums[i];
-                cnt1 = 1;
-            } else if(cnt2 == 0){
-                el2 = nums[i];
-                cnt2 = 1;
+            } else if(nums[i] == ele1){
+                cnt1++;
+            } else if(nums[i] == ele2){
+                cnt2++;
             } else {
                 cnt1--;
                 cnt2--;
             }
         }
-        
-        List<Integer> ans = new ArrayList<>();
+
         cnt1 = 0; cnt2 = 0;
-        
+        List<Integer> list = new ArrayList<>();
+
         for(int i=0; i<n; i++){
-            if(nums[i] == el1){
+            if(nums[i] == ele1){
                 cnt1++;
-            } else if(nums[i] == el2){
+            } else if(nums[i] == ele2){
                 cnt2++;
             }
         }
-        
-        if(cnt1 > n/3){
-            ans.add(el1);
-        }
-        
-        if(cnt2 > n/3){
-            ans.add(el2);
-        }
-        
-        return ans;
+
+        if(cnt1 > n/3)
+            list.add(ele1);
+
+        if(cnt2 > n/3)
+            list.add(ele2);
+
+         return list;       
     }
 }
