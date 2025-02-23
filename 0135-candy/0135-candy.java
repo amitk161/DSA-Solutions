68882fdde31639bbd33b193353cc39eebd08e2a8
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2025-02-23 20:35:19 +0530

    Time: 3 ms (83.39%), Space: 45.8 MB (66.59%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -2,8 +2,7 @@ class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
         int[] left = new int[n];
-        int[] right = new int[n];
-        left[0] = 1; right[n-1] = 1;
+        left[0] = 1;
 
         for(int i=1; i<n; i++){
             if(ratings[i] > ratings[i-1]){
@@ -13,18 +12,16 @@ class Solution {
             }
         }
 
+        int right = 1, sum = left[n-1];
         for(int i=n-2; i>=0; i--){
             if(ratings[i] > ratings[i+1]){
-                right[i] = right[i+1] + 1;
+                right++;
             } else {
-                right[i] = 1;
+                right = 1;
             }
+            sum += Math.max(left[i], right);
         }
 
-        int sum = 0;
-        for(int i=0; i<n; i++){
-            sum += Math.max(left[i], right[i]);
-        }
         return sum;
     }
 }
