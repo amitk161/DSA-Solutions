f15f10045d9d23f1666f49f4660728aa5dc1fdfb
AMIT KUMAR <a.kamitkumar1920@gmail.com>
2026-05-30 13:56:32 +0530

    Time: 48 ms (7.51%), Space: 47.7 MB (7.02%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,23 +1,26 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int maxLen = 0, maxf = 0;
+        int maxLen = 0, maxF =  0;
         HashMap<Character, Integer> map = new HashMap<>();
         int l = 0, r = 0;
 
         while(r < s.length()){
             map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
-            maxf = Math.max(maxf, map.get(s.charAt(r)));
+            maxF = Math.max(maxF, map.get(s.charAt(r)));
 
-            if(r-l+1 - maxf > k){
+            while(r-l+1 - maxF > k){
                 map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
                 if(map.get(s.charAt(l)) == 0)
                     map.remove(s.charAt(l));
-                maxf = 0;   
+                maxF = 0;
 
-                l++;
+                for(Map.Entry<Character, Integer> it: map.entrySet()){
+                    maxF = Math.max(maxF, it.getValue());
+                } 
+                l++;   
             }
 
-            if(r-l+1 - maxf <= k){
+            if(r-l+1 - maxF <= k){
                 maxLen = Math.max(maxLen, r-l+1);
             }
             r++;
